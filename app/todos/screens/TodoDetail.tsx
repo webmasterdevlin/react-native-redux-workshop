3ddb78020bc1c01195838f98a0251d2eeb13f9cb
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-25 11:20:35 +0100

    updated

---
+++ app/todos/screens/TodoDetail.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native-paper';
+
+const TodoDetail: React.FC<any> = props => {
+  return (
+    <Text
+      style={{
+        fontSize: 30,
+      }}>
+      TodoDetail Works!
+    </Text>
+  );
+};
+export default TodoDetail;
