7b430150f843b0160ce23396e4aee95f69305bbd
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-25 10:46:35 +0100

    updated

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native'
-import { Button } from 'react-native-paper';
+import { View, StyleSheet, StatusBar } from 'react-native'
+import { Headline, Paragraph, TextInput, Button } from 'react-native-paper';
 
-const LoginScreen = () => {
+const LoginScreen: React.FC = () => {
     return (<View>
-        <Text>Hello LoginScreen</Text>
-        <Text>Hello LoginScreen</Text>
-        <Text>Hello LoginScreen</Text>
-        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-            Press me
-        </Button>
+        
     </View>)
 };
 export default LoginScreen;
+
+
+const styles = StyleSheet.create({
+    base: {
+        flex: 1,
+        paddingLeft: 16,
+        paddingRight: 16,
+        alignContent: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff',
+    },
+    divider: {
+        height: 16,
+    },
+    headline: {
+        fontSize: 30,
+    },
+    appDesc: {
+        textAlign: 'center',
+    },
+    header: {
+        padding: 32,
+    },
+    appTitle: {
+        textAlign: 'center',
+        fontSize: 35,
+        lineHeight: 35,
+        fontWeight: '700',
+    },
+    btn: {
+        height: 50,
+        paddingTop: 6,
+        marginBottom: 12,
+    },
+});
