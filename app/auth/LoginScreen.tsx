04167b6f55f94046b8035f83dffec034cc3f05f1
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-25 11:00:59 +0100

    updated

--- app/auth/LoginScreen.tsx
+++ app/auth/LoginScreen.tsx
@@ -1,85 +1,78 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar } from 'react-native'
-import { Headline, Paragraph, TextInput, Button } from 'react-native-paper';
+import {View, StyleSheet, StatusBar} from 'react-native';
+import {Headline, Paragraph, TextInput, Button} from 'react-native-paper';
 
 const LoginScreen: React.FC = () => {
-      return (
-        <View style={styles.base}>
-            <>
-                <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
-            </>
+  return (
+    <View style={styles.base}>
+      <>
+        <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
+      </>
 
-            <View style={styles.header}>
-                <Headline style={styles.appTitle}>TodoApp</Headline>
-                <Paragraph style={styles.appDesc}>
-                    Inmeta React Native Course.
+      <View style={styles.header}>
+        <Headline style={styles.appTitle}>TodoApp</Headline>
+        <Paragraph style={styles.appDesc}>
+          Inmeta React Native Course.
         </Paragraph>
-            </View>
+      </View>
 
-            <>
-                <View style={styles.divider} />
-                <TextInput onChange={() => { }} label="*Username or email" />
-            </>
+      <>
+        <View style={styles.divider} />
+        <TextInput onChange={() => {}} label="*Username or email" />
+      </>
 
-            <>
-                <View style={styles.divider} />
-                <TextInput label="*Password" secureTextEntry />
-            </>
+      <>
+        <View style={styles.divider} />
+        <TextInput label="*Password" secureTextEntry />
+      </>
 
-            <>
-                <View style={styles.divider} />
-                <Button
-                    disabled={false}
-                    style={styles.btn}
-                    mode="contained">
-                    Todo
+      <>
+        <View style={styles.divider} />
+        <Button disabled={false} style={styles.btn} mode="contained">
+          Todo
         </Button>
 
-                <Button
-                    disabled={false}
-                    style={styles.btn}
-                    mode="outlined">
-                    Food
+        <Button disabled={false} style={styles.btn} mode="outlined">
+          Food
         </Button>
-                <View style={styles.divider} />
-                <View style={styles.divider} />
-            </>
-        </View>
-    );
+        <View style={styles.divider} />
+        <View style={styles.divider} />
+      </>
+    </View>
+  );
 };
 export default LoginScreen;
 
-
 const styles = StyleSheet.create({
-    base: {
-        flex: 1,
-        paddingLeft: 16,
-        paddingRight: 16,
-        alignContent: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#ffffff',
-    },
-    divider: {
-        height: 16,
-    },
-    headline: {
-        fontSize: 30,
-    },
-    appDesc: {
-        textAlign: 'center',
-    },
-    header: {
-        padding: 32,
-    },
-    appTitle: {
-        textAlign: 'center',
-        fontSize: 35,
-        lineHeight: 35,
-        fontWeight: '700',
-    },
-    btn: {
-        height: 50,
-        paddingTop: 6,
-        marginBottom: 12,
-    },
+  base: {
+    flex: 1,
+    paddingLeft: 16,
+    paddingRight: 16,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  divider: {
+    height: 16,
+  },
+  headline: {
+    fontSize: 30,
+  },
+  appDesc: {
+    textAlign: 'center',
+  },
+  header: {
+    padding: 32,
+  },
+  appTitle: {
+    textAlign: 'center',
+    fontSize: 35,
+    lineHeight: 35,
+    fontWeight: '700',
+  },
+  btn: {
+    height: 50,
+    paddingTop: 6,
+    marginBottom: 12,
+  },
 });
