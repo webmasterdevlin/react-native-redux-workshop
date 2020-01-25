cddc49fa373d24d45888dda85652f51180c138e5
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-25 11:13:27 +0100

    updated

--- app/navigation/root-navigation.tsx
+++ app/navigation/root-navigation.tsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
-import { NavigationNativeContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationNativeContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from '../auth/LoginScreen';
+import TodoList from '../todos/screens/TodoList';
 
 const Stack = createStackNavigator();
 
 const RootNavigation = () => {
-    return (
-        <NavigationNativeContainer>
-            <Stack.Navigator initialRouteName="loginScreen">
-                <Stack.Screen
-                    name="loginScreen"
-                    component={LoginScreen} />
-                    <Stack.Screen 
-                        name="todoList"
-                        component={}
-                    />
-            </Stack.Navigator>
-        </NavigationNativeContainer>
-    );
+  return (
+    <NavigationNativeContainer>
+      <Stack.Navigator initialRouteName="loginScreen">
+        <Stack.Screen
+          name="loginScreen"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="todoList"
+          component={TodoList}
+          options={{title: 'My Todo List'}}
+        />
+      </Stack.Navigator>
+    </NavigationNativeContainer>
+  );
 };
 export default RootNavigation;
