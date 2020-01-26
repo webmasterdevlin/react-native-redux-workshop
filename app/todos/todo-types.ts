47dee46472cb8b6bf61d3ea5d359506917a29042
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-26 09:21:28 +0100

    updated

---
+++ app/todos/todo-types.ts
@@ -0,0 +1,21 @@
+export interface ITodoShape {
+  readonly todos: ITodoModel[];
+  readonly todo: ITodoModel;
+  readonly isLoading: boolean;
+  readonly error: string;
+}
+
+export type ApiResponse = Record<string, any>;
+
+export interface ITodoModel extends ApiResponse {
+  id: string;
+  title: string;
+}
+
+/* action types */
+export const TodoActionTypes = {
+  FETCH_TODOS_REQUEST: '@@/todo/FETCH_TODOS_REQUEST',
+  FETCH_TODOS_SUCCESS: '@@/todo/FETCH_TODOS_SUCCESS',
+  FETCH_TODOS_FAIL: '@@/todo/FETCH_TODOS_FAIL',
+  
+};
