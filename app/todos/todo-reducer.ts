26048d62d22d8575bcba1ac77e48c60c3005e0e7
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-26 10:47:49 +0100

    updated

--- app/todos/todo-reducer.ts
+++ app/todos/todo-reducer.ts
@@ -24,6 +24,16 @@ export const todoReducer = (
       return {...state, isLoading: false, todos: action.payload};
     case TodoActionTypes.FETCH_TODOS_FAIL:
       return {...state, isLoading: false, error: action.payload};
+    case TodoActionTypes.REMOVE_TODO_FAIL:
+      return {...state, isLoading: true};
+    case TodoActionTypes.REMOVE_TODO_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        todos: state.todos.filter(t => t.id !== action.payload),
+      };
+    case TodoActionTypes.REMOVE_TODO_FAIL:
+      return {...state, isLoading: false, error: action.payload};
 
     default:
       return state;
--- app/todos/todo-saga.ts
+++ app/todos/todo-saga.ts
@@ -1,7 +1,7 @@
 import {put, takeEvery, call} from 'redux-saga/effects';
 import {all} from '@redux-saga/core/effects';
 import {TodoActionTypes} from './todo-types';
-import {getTodos} from './todo-service';
+import {getTodos, deleteTodo} from './todo-service';
 
 /* function generator implementations of Saga */
 function* fetchingTodos() {
@@ -15,18 +15,28 @@ function* fetchingTodos() {
     });
   }
 }
-
+// renaming the payload with id
 function* removingTodo({payload: id}): any {
-    try {
-        yield call(deleteTodo)
-    }
+  try {
+    yield call(deleteTodo, id);
+    yield put({type: TodoActionTypes.REMOVE_TODO_SUCCESS, payload: id});
+  } catch (e) {
+    yield put({
+      type: TodoActionTypes.REMOVE_TODO_FAIL,
+      payload: e.message,
+    });
+  }
 }
 
 /* Saga watches actions */
 function* watchFetchingTodos() {
   yield takeEvery(TodoActionTypes.FETCH_TODOS_REQUEST, fetchingTodos);
 }
+function* watchRemovingTodo() {
+  yield takeEvery(TodoActionTypes.REMOVE_TODO_REQUEST, removingTodo);
+}
+
 /* Saga sends all the watchers to the sagaMiddleware to run */
 export function* todoSaga() {
-  yield all([watchFetchingTodos()]);
+  yield all([watchFetchingTodos(), watchRemovingTodo()]);
 }
