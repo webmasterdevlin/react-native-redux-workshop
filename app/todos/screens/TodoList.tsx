1d0ecef33ad68e25ba807291d50cfc5cc7bc8420
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-26 11:39:40 +0100

    updated

--- app/todos/screens/TodoList.tsx
+++ app/todos/screens/TodoList.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import {View, StyleSheet, Alert} from 'react-native';
-import {Button, Title, ActivityIndicator} from 'react-native-paper';
+import {
+  Button,
+  Title,
+  ActivityIndicator,
+  TextInput,
+  HelperText,
+} from 'react-native-paper';
 
 import {useSelector, useDispatch} from 'react-redux';
 
@@ -10,6 +16,9 @@ import {ITodoModel} from '../todo-types';
 import {Dispatch} from 'redux';
 import {IApplicationState} from '../../store';
 
+import {Formik, FormikProps} from 'formik';
+import * as Yup from 'yup';
+
 const TodoList: React.FC<any> = props => {
   /* part of Redux pattern */
   const dispatch: Dispatch = useDispatch();
@@ -17,12 +26,48 @@ const TodoList: React.FC<any> = props => {
     (state: IApplicationState) => state.todoReducer,
   );
 
+  const [todo, setTodo] = React.useState<ITodoModel>({
+    title: '',
+  } as ITodoModel);
+
   React.useEffect(() => {
     dispatch(fetchTodos());
   }, []);
 
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .label('Title')
+      .min(4, 'Title must have at least 4 characters')
+      .max(120)
+      .required(),
+  });
+
   return (
     <View style={styles.container}>
+      <View style={{marginBottom: 20}}>
+        <Formik
+          initialValues={todo}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => {
+            //  TODO: dispatch(an action)
+            actions.resetForm();
+          }}>
+          {formikProps => (
+            <View>
+              <TextInput
+                onChangeText={formikProps.handleChange('title')}
+                onBlur={formikProps.handleBlur('title')}
+                value={formikProps.values.title}
+                label="what's new?"
+              />
+              <HelperText type={'error'}>{formikProps.errors.title}</HelperText>
+              <Button mode="contained" onPress={formikProps.handleSubmit}>
+                Save
+              </Button>
+            </View>
+          )}
+        </Formik>
+      </View>
       <View style={styles.list}>
         {isLoading ? (
           <View style={styles.loaderBase}>
