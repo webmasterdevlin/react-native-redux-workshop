d39ff8eeedbaa7c39af431a4cf958e3524f33eda
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-01-26 09:36:11 +0100

    updated

--- app/todos/todo-saga.ts
+++ app/todos/todo-saga.ts
@@ -1,10 +1,17 @@
 import {put, takeEvery, call} from 'redux-saga/effects';
 import {all} from '@redux-saga/core/effects';
 import {TodoActionTypes} from './todo-types';
+import {getTodos} from './todo-service';
 
 /* function generator implementations of Saga */
 function* fetchingTodos() {
-    try {
-        const {data} = //HTTP
-    }
+  try {
+    const {data} = yield call(getTodos); // saga
+    yield put({type: TodoActionTypes.FETCH_TODOS_SUCCESS, payload: data});
+  } catch (e) {
+    yield put({
+      type: TodoActionTypes.FETCH_TODOS_FAIL,
+      payload: e.message,
+    });
+  }
 }
